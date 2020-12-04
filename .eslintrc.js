bad89b5d71d548515b3b52c4daad8929f57c073b
Maya Saringan <mayas.saringan@gmail.com>
2020-12-04 14:16:56 -0500

    Linted project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,21 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {functions: true, classes: true, variables: false},
+    ], // disable the rule for variables, but enable it for functions and classes
+    '@typescript-eslint/no-shadow': 'off',
+    'no-fallthrough': 'off',
+  },
 };
